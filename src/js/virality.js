7a432b8bbd1c45a0552925d347781129abbd0349
Chris Dary <umbrae@gmail.com>
2012-03-22 08:06:43 -0400

    Nodes actually work now.

--- src/js/virality.js
+++ src/js/virality.js
@@ -1,4 +1,4 @@
-var NUM_USERS = 500,
+var NUM_USERS = 30,
     MIN_CONNECTIONS = 1,
     MAX_CONNECTIONS = NUM_USERS * 0.1;
 
@@ -74,7 +74,7 @@ window.virality = (function($) {
         if (userId in users) {
             return users[userId];
         } else {
-            node = sys.addNode(userId);
+            node = sys.addNode(userId, {"shape": "dot", "color": "#9999CC"});
             node.p = arbor.Point(Math.random(), Math.random());
             users[userId] = node;
             newUsers[userId] = node;
@@ -91,15 +91,16 @@ window.virality = (function($) {
     }
 
     function startTick() {
-        addUser('1');
         window.setTimeout(function() {
-            tickInterval = window.setInterval(tick, 500);
-        }, 500);
+            tickInterval = window.setInterval(tick, 10);
+        }, 0);
     }
 
     function tick() {
-        console.log("tick")
         sys.renderer.redraw();
+        if (Object.keys(users).length == 0) {
+            addUser('1');
+        }
         for (var userId in newUsers) {
             if (newUsers.hasOwnProperty(userId)) {
                 addRandomConnections(newUsers[userId]);
@@ -109,80 +110,16 @@ window.virality = (function($) {
         if (Object.keys(users).length > NUM_USERS) {
             console.log("Stopping");
             window.clearInterval(tickInterval);
+            sys.stop();
+            
             return;
         }
     }
     
     function init() {
-        var DeadSimpleRenderer = function(canvas){
-          var canvas = $(canvas).get(0)
-          var ctx = canvas.getContext("2d");
-          var particleSystem = null
-
-          var that = {
-            //
-            // the particle system will call the init function once, right before the
-            // first frame is to be drawn. it's a good place to set up the canvas and
-            // to pass the canvas size to the particle system
-            //
-            init:function(system){
-              console.log("Init called");
-              // save a reference to the particle system for use in the .redraw() loop
-              particleSystem = system
-
-              // inform the system of the screen dimensions so it can map coords for us.
-              // if the canvas is ever resized, screenSize should be called again with
-              // the new dimensions
-              particleSystem.screenSize(canvas.width, canvas.height) 
-              particleSystem.screenPadding(0) // leave an extra 80px of whitespace per side
-            },
-
-            // 
-            // redraw will be called repeatedly during the run whenever the node positions
-            // change. the new positions for the nodes can be accessed by looking at the
-            // .p attribute of a given node. however the p.x & p.y values are in the coordinates
-            // of the particle system rather than the screen. you can either map them to
-            // the screen yourself, or use the convenience iterators .eachNode (and .eachEdge)
-            // which allow you to step through the actual node objects but also pass an
-            // x,y point in the screen's coordinate system
-            // 
-
-            redraw:function(){
-              ctx.clearRect(0,0, canvas.width, canvas.height)
-
-              particleSystem.eachEdge(function(edge, pt1, pt2){
-                // edge: {source:Node, target:Node, length:#, data:{}}
-                // pt1:  {x:#, y:#}  source position in screen coords
-                // pt2:  {x:#, y:#}  target position in screen coords
-
-                // draw a line from pt1 to pt2
-                ctx.strokeStyle = "rgba(0,0,0, .3)"
-                ctx.lineWidth = 1 + 4*edge.data.weight
-                ctx.beginPath()
-                ctx.moveTo(pt1.x, pt1.y)
-                ctx.lineTo(pt2.x, pt2.y)
-                ctx.stroke()
-              })
-
-              particleSystem.eachNode(function(node, pt){
-                // node: {mass:#, p:{x,y}, name:"", data:{}}
-                // pt:   {x:#, y:#}  node position in screen coords
-
-                // draw a circle centered at pt
-                var w = 4
-                ctx.beginPath(); 
-                ctx.fillStyle = "black"
-                ctx.arc(pt.x, pt.y, w, 0, Math.PI*2, true);
-                ctx.closePath();
-                ctx.fill();
-              })
-            }
-          }
-          return that
-        }
-
-        sys = arbor.ParticleSystem(1000, 400, 1);
-        sys.renderer = DeadSimpleRenderer(canvas);
+        sys = arbor.ParticleSystem(2600, 512, 0.5);
+        sys.renderer = Renderer(canvas);
+        sys.fps(30);
         startTick();
     }
     
