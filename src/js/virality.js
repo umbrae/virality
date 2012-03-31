0832315c6fb14a8cafaef687305421a0967fefb7
Chris Dary <umbrae@gmail.com>
2012-03-30 20:14:39 -0400

    Ranges, tick counting, clear graph.

--- src/js/virality.js
+++ src/js/virality.js
@@ -1,8 +1,4 @@
-var NUM_USERS = 50,
-    MIN_CONNECTIONS = 1,
-    MAX_CONNECTIONS = NUM_USERS * 0.02,
-    STICKINESS = 0.95,
-    USER_COLOR = '#3366CC',
+var USER_COLOR = '#3366CC',
     ACTIVE_USER_COLOR = '#CC6633',
     PAST_USER_COLOR = '#CCCCCC';
 
@@ -26,9 +22,26 @@ window.virality = (function($) {
     var users = {},
         pastUsers = {},
         activeUsers = {},
+        config = {},
         sys,
         canvas = $('#social-graph'),
+        tickCounter = 0,
         tickInterval;
+
+    function getConfig(configVar) {
+        var configVal;
+
+        if (!config.hasOwnProperty(configVar)) {
+            configVal = $('#' + configVar).val();
+            if (!isNaN(parseFloat(configVal)) && isFinite(configVal)) {
+                config[configVar] = parseFloat(configVal);
+            } else {
+                config[configVar] = configVal;
+            }
+        }
+        
+        return config[configVar];
+    }
 
     function addUser(userId) {
         if (userId in users) {
@@ -41,11 +54,11 @@ window.virality = (function($) {
     }
 
     function randomUser() {
-        return users[randRange(1, NUM_USERS-1)]
+        return users[randRange(1, getConfig('population')-1)]
     }
 
     function activateUser(user) {
-        sys.tweenNode(user, 0.25, {'color': ACTIVE_USER_COLOR})
+        sys.tweenNode(user, 0.1, {'color': ACTIVE_USER_COLOR})
         activeUsers[user.name] = user;
         pastUsers[user.name] = user;
     }
@@ -55,16 +68,36 @@ window.virality = (function($) {
         delete activeUsers[user.name];
     }
 
+    /**
+     * User source tells user target about the concept.
+     * If it sticks, return true. If it doesn't, return false;
+    **/
+    function tellUser(sourceUser, targetUser) {
+        var interested = Math.random();
+        
+        if (pastUsers.hasOwnProperty(targetUser.name)) {
+            return false;
+        }
+
+        console.log(interested);
+        console.log(getConfig('spread-odds'));
+        if (interested < getConfig('spread-odds')) {
+            return true;
+        }
+        
+        return false;
+    }
+
     function setupNetwork() {
         /* Create the nodes */
-        for(var userId=0; userId < NUM_USERS; userId++) {
+        for(var userId=0; userId < getConfig('population'); userId++) {
             addUser(userId);
         }
 
         /* Add the edges */
-        for(var userId=0; userId < NUM_USERS; userId++) {
+        for(var userId=0; userId < getConfig('population'); userId++) {
             curUser = users[userId];
-            numEdges = Math.round(powerlaw(MIN_CONNECTIONS, MAX_CONNECTIONS, 2));
+            numEdges = Math.round(powerlaw(1, getConfig('connection-factor'), 2));
             for(var edge=0; edge < numEdges; edge++) {
                 sys.addEdge(curUser, randomUser());
             }
@@ -74,12 +107,17 @@ window.virality = (function($) {
     }
 
     function startTick() {
-        // Activate one user to seed the system
+        $('#tick-counter').text(tickCounter);
+
+        // Seed the system if we have no active users yet
         if (Object.keys(activeUsers).length == 0) {
-            activateUser(randomUser());            
+            for(var i=0; i < getConfig('start-users'); i++) {
+                activateUser(randomUser());
+            }
         }
         
-        tickInterval = window.setInterval(tick, 1000);
+        window.clearInterval(tickInterval);
+        tickInterval = window.setInterval(tick, getConfig('tick-speed'));
     }
 
     function stopTick() {
@@ -87,6 +125,13 @@ window.virality = (function($) {
     }
 
     function tick() {
+        tickCounter++;
+        $('#tick-counter').text(tickCounter);
+        
+        if(Object.keys(activeUsers).length == 0) {
+            stopTick();
+        }
+        
         for(activeUserId in activeUsers) {
             if (!activeUsers.hasOwnProperty(activeUserId)) {
                 continue;
@@ -94,7 +139,7 @@ window.virality = (function($) {
 
             var activeUser = activeUsers[activeUserId];
 
-            if (Math.random() > STICKINESS) {
+            if (Math.random() > getConfig('stickiness')) {
                 /* This user lost interest in the product and stopped using it. */
                 deactivateUser(activeUser);
             } else {
@@ -104,14 +149,14 @@ window.virality = (function($) {
 
                 for (var e=0; e < outEdges.length; e++) {
                     var edge = outEdges[e];
-                    if (!pastUsers.hasOwnProperty(edge.target.name)) {
-                        activateUser(edge.target);                            
+                    if (tellUser(edge.source, edge.target)) {
+                        activateUser(edge.target);
                     }
                 }
                 
                 for (var e=0; e < inEdges.length; e++) {
                     var edge = inEdges[e];
-                    if (!pastUsers.hasOwnProperty(edge.source.name)) {
+                    if (tellUser(edge.target, edge.source)) {
                         activateUser(edge.source);
                     }
                 }
@@ -128,16 +173,47 @@ window.virality = (function($) {
         stopTick();
         return false;
     }
+
+    function clear() {
+        stop();
+        users = {};
+        activeUsers = {};
+        pastUsers = {};
+        config = {};
+        tickCounter = 0;
+        $('#tick-counter').text(tickCounter);
+        sys.eachNode(function(node) {
+            sys.pruneNode(node);
+        });
+        setupNetwork();
+        return false;
+    }
+
+    function setupRanges() {
+        $('span.range-display').each(function() {
+            var $d = $(this),
+                $range = $('#' + $(this).data('for'));
+
+            $range.change(function() {
+                if($d.data('percent')) {
+                    $d.text(parseInt($range.val()*100) + "%");
+                } else {
+                    $d.text($range.val());
+                }
+            });
+            $range.trigger('change');
+        });
+    }
     
     function init() {
         sys = arbor.ParticleSystem(2600, 512, 0.5);
         sys.renderer = Renderer(canvas);
-        sys.fps(30);
+        setupRanges();
         setupNetwork();
 
         $('#play').click(start);
         $('#stop').click(stop);
-        $('#setup').click(setupNetwork);
+        $('#setup').click(clear);
     }
     
     return {
